173cad6158bd674d66ed4252b5ac0601a35998d1
Jamep <L20060904@chihuahua.tecnm.mx>
2023-10-09 11:21:31 -0600

    Habilitar_Pullup

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ uint8_t *p2_sel0 = (uint8_t*)0x040004c0B;
 uint8_t *p2_sel1 = (uint8_t*)0x040004c0d;
 uint8_t *p2_out = (uint8_t*)0x040004c03;
 uint8_t *p2_dir = (uint8_t*)0x040004c05;
+uint8_t *p2_ren = (uint8_t*)0x040004c07;
 
 //Codigo de colores
 /*
@@ -20,10 +21,11 @@ uint8_t *p2_dir = (uint8_t*)0x040004c05;
 void main(void){
     uint32_t i; //Para el ciclo for
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
-    *p2_sel0 &= ~0x01;
-    *p2_sel1 &= ~0x01; //P2 en GPIO
+    *p2_sel0 &= ~0x0F;
+    *p2_sel1 &= ~0x0F; //P2 en GPIO
 
-    *p2_dir |= 0x03; //Se habilita dos bits de entradas (G,R)
+    *p2_dir ^= 0x03; //Se habilita dos bits de entradas (G,R)
+    *p2_ren |= 0x08; //Se habilita el pullup en P2.4
 
     *p2_out ^= 0x03; //Se asigna un 2 (Verde)
     while(1){
